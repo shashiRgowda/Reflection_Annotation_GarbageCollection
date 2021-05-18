a633bf6030d9b471d91eaa5e01345c7e45f2975d
shashiRgowda <shashikumartr64@gmail.com>
2021-05-18 16:32:55 +0530

    Added:AnnotationsDemo

---
+++ AnnotationDemo/AnnotationDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnnotationDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AnnotationsTest.Display();
+        }
+    }
+}
---
+++ AnnotationDemo/AnnotationTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AnnotationTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AnnotationTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("92a803c6-c5fd-4f51-ac95-6c5d4e67a5ab")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
